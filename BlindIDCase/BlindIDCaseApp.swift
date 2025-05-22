263bdf32ab6905c964be60af0c9f55d278a0e65c
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-05-22 09:18:26 +0300

    Initial Commit

---
+++ BlindIDCase/BlindIDCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindIDCaseApp.swift
+//  BlindIDCase
+//
+//  Created by Onur Altintas on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlindIDCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindIDCase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlindIDCase
+//
+//  Created by Onur Altintas on 22.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
