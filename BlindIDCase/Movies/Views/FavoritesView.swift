c92420178aeeedcf784c9e85f5c31c36435a5c2e
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-05-25 15:10:56 +0300

    Favoriler sayfası EmptyView ve loading animasyonu gösterecek şekilde düzeltildi.

--- BlindIDCase/Movies/Views/FavoritesView.swift
+++ BlindIDCase/Movies/Views/FavoritesView.swift
@@ -8,27 +8,46 @@
 import SwiftUI
 
 struct FavoritesView: View {
-    @ObservedObject var viewModel: HomeViewModel
     
+    // MARK: - Properties
+    @ObservedObject var viewModel: HomeViewModel
+
+    // MARK: - Body
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(spacing: 16) {
-                    ForEach(viewModel.favoriteMovies) { movie in
-                        NavigationLink(destination: MovieDetailView(movie: movie)) {
-                            MovieCardView(movie: movie)
-                                .padding(.horizontal)
+            ZStack {
+                // MARK: - Movie List, EmptyView
+                ScrollView {
+                    VStack(spacing: 16) {
+                        if viewModel.favoriteMovies.isEmpty {
+                            EmptyStateView(
+                                title: "No Favorites",
+                                message: "You haven't added any favorites yet.",
+                                systemImageName: "heart.slash"
+                            )
+                            .padding(.top, 100)
+                        } else {
+                            ForEach(viewModel.favoriteMovies) { movie in
+                                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                                    MovieCardView(movie: movie)
+                                        .padding(.horizontal)
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                            }
                         }
-                        .buttonStyle(PlainButtonStyle())
                     }
+                    .padding(.top)
                 }
-                .padding(.top)
-            }
-            .padding(.bottom)
-            .background(Color.black.opacity(0.1))
-            .navigationTitle("Favorites")
-            .onAppear {
-                viewModel.refreshIsLikedStatesFromUser()
+                .padding(.bottom)
+                .background(Color.black.opacity(0.1))
+                .navigationTitle("Favorites")
+                .onAppear {
+                    viewModel.refreshIsLikedStatesFromUser()
+                }
+
+                if viewModel.isFetchingData {
+                    LoadingOverlayView()
+                }
             }
         }
     }
